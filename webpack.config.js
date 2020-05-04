4872230c07e613c9295231e16d12c9658c024223
jaysongforever <526678604@qq.com>
2020-05-05 04:59:00 +0800

    'webpack'

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,3 @@
-const path = require('path')
-
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'webpack-numbers.js',
-    path: path.resolve(__dirname, 'dist'),
-    library: {
-      name: 'webpackNumbers',
-      type: 'umd',
-    },
-  },
-  externals: {
-    lodash: {
-      commonjs: 'lodash',
-      commonjs2: 'lodash',
-      amd: 'lodash',
-      root: '_',
-    },
-  },
-}
+2020-05-05T04:59:00+08:00
+随机数:59132
+提交次数:1
