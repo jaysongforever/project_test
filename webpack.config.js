6514132ccc4c3652ebfa261e174cdc79ea650544
JaySong <526678604@qq.com>
2021-05-30 19:10:51 +0800

    test

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'webpack-numbers.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: {
+      name: 'webpackNumbers',
+      type: 'umd',
+    },
+  },
+  externals: {
+    lodash: {
+      commonjs: 'lodash',
+      commonjs2: 'lodash',
+      amd: 'lodash',
+      root: '_',
+    },
+  },
+}
